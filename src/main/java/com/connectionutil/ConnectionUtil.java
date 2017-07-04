daf0d137959d134c39318265effc339eae2f8e81
bhargavisingh72 <bhargavisingh72@gmail.com>
2017-07-04 16:08:44 +0530

    Jdbc Template

--- src/main/java/com/connectionutil/ConnectionUtil.java
+++ src/main/java/com/connectionutil/ConnectionUtil.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ConnectionUtil {
 	public static final String DRIVER="com.mysql.jdbc.Driver";
-	public static final String URL="jdbc:mysql://localhost:3306/bhargavidb";
+	public static final String URL="jdbc:mysql://localhost:3306/irinedb";
 	public static final String USERNAME="root";
 	public static final String PASSWORD="root";
 	
